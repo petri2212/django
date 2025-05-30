52940f5c6b8a67f871d8b9281143840d25743bbe
Andrei <a.petrisor@studenti.unibg.it>
2025-05-30 13:52:53 +0200

    initialize 2nd pjct

--- exe1/myproject/myproject/views.py
+++ exe1/myproject/myproject/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 def homepage(request):
   #  return HttpResponse("Hello World! im petri")
-  return render(request, 'home.html')
+  return render(request, 'home.php')
 
 def about(request):
   #  return HttpResponse("Sei in about")
-  return render(request, 'about.html')
+  return render(request, 'about.php')
